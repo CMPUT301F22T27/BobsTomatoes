50e54146080aa7cb8f3a05e5b5169b57c59c6e92
ggiang1 <giang1@ualberta.ca>
2022-10-29 16:32:43 -0600

    Created Ingredient Database

--- BobsTomatoes/app/src/main/java/com/example/bobstomatoes/MainActivity.java
+++ BobsTomatoes/app/src/main/java/com/example/bobstomatoes/MainActivity.java
@@ -8,11 +8,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
-    //FirebaseFirestore db = FirebaseFirestore.getInstance();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
