f758e926e1ea99e603a12a1532e53e7bc1002e92
jggeiger <jggeiger@ualberta.ca>
2022-10-31 21:04:10 -0600

    implemented navigation bar

--- BobsTomatoes/app/src/main/java/com/example/bobstomatoes/IngredientStorageActivity.java
+++ BobsTomatoes/app/src/main/java/com/example/bobstomatoes/IngredientStorageActivity.java
@@ -3,12 +3,24 @@ package com.example.bobstomatoes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class IngredientStorageActivity extends AppCompatActivity {
+public class IngredientStorageActivity extends AbstractNavigationBar {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient_storage);
+
+        //Sets up buttons and onClickListeners for navigation bar
+        initializeButtons(IngredientStorageActivity.this);
+
+        storageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //do nothing
+            }
+        });
+
     }
 }
--- BobsTomatoes/app/src/main/java/com/example/bobstomatoes/ShoppingListActivity.java
+++ BobsTomatoes/app/src/main/java/com/example/bobstomatoes/ShoppingListActivity.java
@@ -3,12 +3,24 @@ package com.example.bobstomatoes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class ShoppingListActivity extends AppCompatActivity {
+public class ShoppingListActivity extends AbstractNavigationBar {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        //Sets up buttons and onClickListeners for navigation bar
+        initializeButtons(ShoppingListActivity.this);
+
+        shoppingListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //do nothing
+            }
+        });
+
     }
 }
