7a298eff0d7aa4bf42adbdee781297ad3e8a5fd7
jggeiger <jggeiger@ualberta.ca>
2022-11-04 15:23:02 -0600

    added intent testing for ingredient storage and recipe

--- BobsTomatoes/app/src/androidTest/java/com/example/bobstomatoes/IngredientStorageActivityTest.java
+++ BobsTomatoes/app/src/androidTest/java/com/example/bobstomatoes/IngredientStorageActivityTest.java
@@ -1,89 +1,333 @@
 package com.example.bobstomatoes;
 
+
 import android.app.Activity;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
-import android.widget.EditText;
-import android.widget.ListView;
-import com.robotium.solo.Solo;
+
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+
+
+
 /**
- * Test class for MainActivity. All the UI tests are written here. Robotium test framework is
- used
+ * Test class for IngredientStorageActivity. All the UI tests are written here. Espresso test framework is used
  */
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest{
-    private Solo solo;
-    @Rule
-    public ActivityTestRule<MainActivity> rule =
-            new ActivityTestRule<>(MainActivity.class, true, true);
-    /**
-     * Runs before all tests and creates solo instance.
-     * @throws Exception
-     */
-    @Before
-    public void setUp() throws Exception{
+public class IngredientStorageActivityTest{
 
-        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
-    }
+    @Rule
+    public ActivityScenarioRule<IngredientStorageActivity> rule =
+            new ActivityScenarioRule<>(IngredientStorageActivity.class);
+
+
+
     /**
-     * Gets the Activity
-     * @throws Exception
+     * Check adding an ingredient to storage
      */
     @Test
-    public void start() throws Exception{
-        Activity activity = rule.getActivity();
+    public void checkSuccessIngredientAdd(){
+
+        onView(withId(R.id.center_add_imageButton_id))
+                .perform((click()));
+
+        onView(withId(R.id.editTextIngredientDesc))
+                .perform(click(), typeText("TEST INGREDIENT"));
+
+        onView(withId(R.id.editTextIngredientDate))
+                .perform(click(), typeText("2022-07-18"));
+
+        onView(withId(R.id.editTextIngredientLocation))
+                .perform(click(), typeText("Fridge"));
+
+        onView(withId(R.id.editTextIngredientAmount))
+                .perform(click(), typeText("1"));
+
+        onView(withId(R.id.editTextIngredientUnit))
+                .perform(click(), typeText("5"));
+
+        onView(withId(R.id.editTextIngredientCategory))
+                .perform(click(), typeText("Water"));
+
+        onView(withText("ADD"))
+                .perform(click());
+
+        //Ensure item is in list and clickable
+        onView(withText("TEST INGREDIENT"))
+                .perform(click());
+
+        //Check for proper entries
+        onView(withId(R.id.editTextIngredientDesc))
+                .check(matches(withText("TEST INGREDIENT")));
+
+        onView(withId(R.id.editTextIngredientDate))
+                .check(matches(withText("2022-07-18")));
+
+        onView(withId(R.id.editTextIngredientLocation))
+                .check(matches(withText("Fridge")));
+
+        onView(withId(R.id.editTextIngredientAmount))
+                .check(matches(withText("1")));
+
+        onView(withId(R.id.editTextIngredientUnit))
+                .check(matches(withText("5")));
+
+        onView(withId(R.id.editTextIngredientCategory))
+                .check(matches(withText("Water")));
+
+
+        //Delete
+        onView(withText("DELETE"))
+                .perform((click()));
+
     }
+
     /**
-     * Add a city to the listview and check the city name using assertTrue
-     * Clear all the cities from the listview and check again with assertFalse
+     * Check editing an ingredient
      */
     @Test
-    public void checkList(){
-//Asserts that the current activity is the MainActivity. Otherwise, show “Wrong Activity”
-        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-        solo.clickOnButton("ADD CITY"); //Click ADD CITY Button
-//Get view for EditText and enter a city name
-        solo.enterText((EditText) solo.getView(R.id.editText_name), "Edmonton");
-        solo.clickOnButton("CONFIRM"); //Select CONFIRM Button
-        solo.clearEditText((EditText) solo.getView(R.id.editText_name)); //Clear the EditText
-/* True if there is a text: Edmonton on the screen, wait at least 2 seconds and
-find minimum one match. */
-        assertTrue(solo.waitForText("Edmonton", 1, 2000));
-        solo.clickOnButton("ClEAR ALL"); //Select ClEAR ALL
-//True if there is no text: Edmonton on the screen
-        assertFalse(solo.searchText("Edmonton"));
+    public void checkSuccessIngredientEdit(){
+
+        //Add item
+        onView(withId(R.id.center_add_imageButton_id))
+                .perform((click()));
+
+        onView(withId(R.id.editTextIngredientDesc))
+                .perform(click(), typeText("TEST INGREDIENT"));
+
+        onView(withId(R.id.editTextIngredientDate))
+                .perform(click(), typeText("2022-07-18"));
+
+        onView(withId(R.id.editTextIngredientLocation))
+                .perform(click(), typeText("Fridge"));
+
+        onView(withId(R.id.editTextIngredientAmount))
+                .perform(click(), typeText("1"));
+
+        onView(withId(R.id.editTextIngredientUnit))
+                .perform(click(), typeText("5"));
+
+        onView(withId(R.id.editTextIngredientCategory))
+                .perform(click(), typeText("Water"));
+
+        onView(withText("ADD"))
+                .perform(click());
+
+        //Click item
+        onView(withText("TEST INGREDIENT"))
+                .perform(click());
+
+        //Edit
+        onView(withId(R.id.editTextIngredientDesc))
+                .perform(click(), clearText(), typeText("TEST INGREDIENT 2"));
+
+        onView(withId(R.id.editTextIngredientDate))
+                .perform(click(), clearText(), typeText("2023-07-18"));
+
+        onView(withId(R.id.editTextIngredientLocation))
+                .perform(click(), clearText(), typeText("Fridge 2"));
+
+        onView(withId(R.id.editTextIngredientAmount))
+                .perform(click(), clearText(), typeText("2"));
+
+        onView(withId(R.id.editTextIngredientUnit))
+                .perform(click(), clearText(), typeText("6"));
+
+        onView(withId(R.id.editTextIngredientCategory))
+                .perform(click(), clearText(), typeText("Water 2"));
+
+        //Edit item
+        onView(withText("EDIT"))
+                .perform(click());
+
+        //Click item
+        onView(withText("TEST INGREDIENT 2"))
+                .perform(click());
+
+        //Check for proper entries
+        onView(withId(R.id.editTextIngredientDesc))
+                .check(matches(withText("TEST INGREDIENT 2")));
+
+        onView(withId(R.id.editTextIngredientDate))
+                .check(matches(withText("2023-07-18")));
+
+        onView(withId(R.id.editTextIngredientLocation))
+                .check(matches(withText("Fridge 2")));
+
+        onView(withId(R.id.editTextIngredientAmount))
+                .check(matches(withText("2")));
+
+        onView(withId(R.id.editTextIngredientUnit))
+                .check(matches(withText("6")));
+
+        onView(withId(R.id.editTextIngredientCategory))
+                .check(matches(withText("Water 2")));
+
+        //Click item
+        onView(withText("TEST INGREDIENT 2"))
+                .perform(click());
+
+        //Delete
+        onView(withText("DELETE"))
+                .perform((click()));
+
     }
+
     /**
-     * Check item taken from the listview
+     * Check deleting an item from ingredient list
      */
     @Test
-    public void checkCiyListItem(){
-        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-        solo.clickOnButton("ADD CITY");
-        solo.enterText((EditText) solo.getView(R.id.editText_name), "Edmonton");
-        solo.clickOnButton("CONFIRM");
-        solo.waitForText("Edmonton", 1, 2000);
-// Get MainActivity to access its variables and methods.
-        MainActivity activity = (MainActivity) solo.getCurrentActivity();
-        final ListView cityList = activity.cityList; // Get the listview
-        String city = (String) cityList.getItemAtPosition(0); // Get item from first position
-        assertEquals("Edmonton", city);
+    public void checkIngredientDelete(){
+
+        //Add item
+        onView(withId(R.id.center_add_imageButton_id))
+                .perform((click()));
+
+        onView(withId(R.id.editTextIngredientDesc))
+                .perform(click(), typeText("TEST INGREDIENT"));
+
+        onView(withId(R.id.editTextIngredientDate))
+                .perform(click(), typeText("2022-07-18"));
+
+        onView(withId(R.id.editTextIngredientLocation))
+                .perform(click(), typeText("Fridge"));
+
+        onView(withId(R.id.editTextIngredientAmount))
+                .perform(click(), typeText("1"));
+
+        onView(withId(R.id.editTextIngredientUnit))
+                .perform(click(), typeText("5"));
+
+        onView(withId(R.id.editTextIngredientCategory))
+                .perform(click(), typeText("Water"));
+
+        onView(withText("ADD"))
+                .perform(click());
+
+        //Click item
+        onView(withText("TEST INGREDIENT"))
+                .perform(click());
+
+        //Delete
+        onView(withText("DELETE"))
+                .perform((click()));
+
+        //Not in list anymore
+        onView(withId(R.id.ingredients_list))
+                .check(matches(not(hasDescendant(withText("TEST INGREDIENT")))));
+
     }
+
     /**
-     * Close activity after each test
-     * @throws Exception
+     * Check adding an invalid ingredient
+     */
+    @Test (expected = Exception.class)
+    public void checkFailureIngredientAdd(){
+
+        //Add empty item
+        onView(withId(R.id.center_add_imageButton_id))
+                .perform((click()));
+
+        onView(withText("ADD"))
+                .perform(click());
+
+    }
+
+    /**
+     * Check editing an invalid ingredient
+     */
+    @Test (expected = Exception.class)
+    public void checkFailureIngredientEdit(){
+
+        //Add item
+        onView(withId(R.id.center_add_imageButton_id))
+                .perform((click()));
+
+        onView(withId(R.id.editTextIngredientDesc))
+                .perform(click(), typeText("TEST INGREDIENT"));
+
+        onView(withId(R.id.editTextIngredientDate))
+                .perform(click(), typeText("2022-07-18"));
+
+        onView(withId(R.id.editTextIngredientLocation))
+                .perform(click(), typeText("Fridge"));
+
+        onView(withId(R.id.editTextIngredientAmount))
+                .perform(click(), typeText("1"));
+
+        onView(withId(R.id.editTextIngredientUnit))
+                .perform(click(), typeText("5"));
+
+        onView(withId(R.id.editTextIngredientCategory))
+                .perform(click(), typeText("Water"));
+
+        onView(withText("ADD"))
+                .perform(click());
+
+        //Click item
+        onView(withText("TEST INGREDIENT"))
+                .perform(click());
+
+        //Edit
+        onView(withId(R.id.editTextIngredientDesc))
+                .perform(click(), clearText());
+
+        onView(withId(R.id.editTextIngredientDate))
+                .perform(click(), clearText());
+
+        onView(withId(R.id.editTextIngredientLocation))
+                .perform(click(), clearText());
+
+        onView(withId(R.id.editTextIngredientAmount))
+                .perform(click(), clearText());
+
+        onView(withId(R.id.editTextIngredientUnit))
+                .perform(click(), clearText());
+
+        onView(withId(R.id.editTextIngredientCategory))
+                .perform(click(), clearText());
+
+        //Try to save edit
+        onView(withText("EDIT"))
+                .perform(click());
+
+    }
+
+    /**
+     * Clear tested items from database
      */
     @After
-    public void tearDown() throws Exception{
-        solo.finishOpenedActivities();
+    public void clear() throws InterruptedException {
+
+        Thread.sleep(15000);
+
+        try {
+
+            onView(withText(containsString("TEST INGREDIENT")))
+                    .perform(click());
+            onView(withText("DELETE"))
+                    .perform(click());
+
+        } catch (Exception e) {}
+
+
     }
+
+
 }
