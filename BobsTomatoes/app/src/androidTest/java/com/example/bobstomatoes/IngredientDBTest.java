a0b923610b93b6338b724187737d5ddcda20414e
ggiang1 <giang1@ualberta.ca>
2022-11-04 12:33:52 -0600

    Completed IngredientDBTest

--- BobsTomatoes/app/src/androidTest/java/com/example/bobstomatoes/IngredientDBTest.java
+++ BobsTomatoes/app/src/androidTest/java/com/example/bobstomatoes/IngredientDBTest.java
@@ -107,8 +107,78 @@ public class IngredientDBTest {
         });
 
         assertEquals(PreSize, ingredientList.size());
+    }
 
-        //Remove the ingredient after the test
+    @Test
+    public void testEditIngredient() {
+        CollectionReference ingredientReference = ingredientDB.getIngredientReference();
+
+        ArrayList<Ingredient> ingredientList = ingredientDB.getIngredientList();
+        Ingredient ingredient = new Ingredient("Mango", "2022-11-04", "Fridge", 6, 6, "Fruit");
+        Ingredient ingredient2 = new Ingredient("Watermelon", "2022-11-04", "Fridge", 6, 6, "Fruit");
+
+        DocumentReference ingredient1Ref = ingredientReference.document(ingredient.getIngredientDesc());
+
+        DocumentReference ingredient2Ref = ingredientReference.document(ingredient2.getIngredientDesc());
+
+        int PreSize = ingredientList.size();
+
+        ingredientDB.addIngredient(ingredient);
+
+        ingredient1Ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if(document.exists()) {
+                        assertEquals(1,1);
+                    } else {
+                        assertEquals(0,1);
+                    }
+                } else {
+                    Log.d("", "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
+        ingredientDB.editIngredient(0,ingredient2);
+
+        ingredient1Ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if(document.exists()) {
+                        assertEquals(0,1);
+                    } else {
+                        assertEquals(1,1);
+                    }
+                } else {
+                    Log.d("", "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
+        ingredient2Ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if(document.exists()) {
+                        assertEquals(1,1);
+                    } else {
+                        assertEquals(0,1);
+                    }
+                } else {
+                    Log.d("", "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
+
+        assertEquals(PreSize+1, ingredientList.size());
+
+        ingredientDB.removeIngredient(ingredient2);
     }
 
 }
