dd75106462c638dd3d11adffa3946743295d40e2
colinchong1 <colin.m.chong@gmail.com>
2022-11-24 18:14:40 -0700

    Created fragment_meal_plan_scale.xml Created MealPlanScaleFragment.java

---
+++ BobsTomatoes/app/src/main/java/com/example/bobstomatoes/MealPlanScaleFragment.java
@@ -0,0 +1,17 @@
+package com.example.bobstomatoes;
+
+import android.widget.EditText;
+
+import androidx.fragment.app.DialogFragment;
+
+/**
+ * Class for the Meal Plan Scale Fragment which allows the user to adjust the number of servings for the meal plan
+ * extends DialogFragment
+ */
+
+public class MealPlanScaleFragment extends DialogFragment {
+
+    private EditText scaleText;
+
+
+}
