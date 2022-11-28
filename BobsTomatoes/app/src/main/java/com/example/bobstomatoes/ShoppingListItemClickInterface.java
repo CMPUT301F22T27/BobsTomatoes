335f5f6b794398966c246050ea6d8e547b68027b
Red <shiraphyre@gmail.com>
2022-11-27 21:20:24 -0700

    Some Code CleanUp

--- BobsTomatoes/app/src/main/java/com/example/bobstomatoes/ShoppingListItemClickInterface.java
+++ BobsTomatoes/app/src/main/java/com/example/bobstomatoes/ShoppingListItemClickInterface.java
@@ -2,6 +2,10 @@ package com.example.bobstomatoes;
 
 import android.view.View;
 
+/**
+ * Shopping List Interface
+ * creates checkboxes
+ */
 public interface ShoppingListItemClickInterface {
     void OnCheckBoxClick(View v, int pos);
 }
