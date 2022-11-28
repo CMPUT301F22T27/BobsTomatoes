b0a178627cb7e6fa3b904169de67cda0b07743f3
ggiang1 <giang1@ualberta.ca>
2022-11-28 14:45:09 -0700

    No zero ingredient amount in recipe or meal plan

--- BobsTomatoes/app/src/main/java/com/example/bobstomatoes/SpecifyIngredientAmountFragment.java
+++ BobsTomatoes/app/src/main/java/com/example/bobstomatoes/SpecifyIngredientAmountFragment.java
@@ -5,13 +5,17 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
@@ -54,6 +58,7 @@ public class SpecifyIngredientAmountFragment extends DialogFragment {
         EditText ingredientAmount = view.findViewById(R.id.ingredientAmount);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        AlertDialog dialog;
         Bundle bundle = this.getArguments();
 
 
@@ -62,18 +67,48 @@ public class SpecifyIngredientAmountFragment extends DialogFragment {
         int ingredientPos = bundle.getInt("selectedIngredientPos");
 
         //Builder for add
-        return builder.setView(view)
+        dialog = builder.setView(view)
                 .setTitle("Input Specified Ingredient Amount")
-                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Add", null)
+                .create();
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                Button button = ((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        ingredient.setIngredientAmount(Integer.parseInt(ingredientAmount.getText().toString()));
-                        ingredientList.set(ingredientPos, ingredient);
-                        listener.onAddIngredientOkPressed(ingredientList);
+                    public void onClick(View view) {
+                        try {
+
+                            if (ingredientAmount.getText().toString().equals("")) {
+                                throw new Exception("Please fill in the ingredient amount");
+                            }
+
+                            if(Integer.parseInt(ingredientAmount.getText().toString()) == 0) {
+                                throw new Exception("0 ingredient amount not allowed");
+                            }
+
+
+                            ingredient.setIngredientAmount(Integer.parseInt(ingredientAmount.getText().toString()));
+                            ingredientList.set(ingredientPos, ingredient);
+                            listener.onAddIngredientOkPressed(ingredientList);
+                            dialog.dismiss();
+                        } catch (Exception e) {
+                            Log.d("Exception", e.toString());
+
+                            Snackbar snackbar = null;
+                            snackbar = snackbar.make(view, e.getMessage(), Snackbar.LENGTH_SHORT);
+                            snackbar.setDuration(600);
+                            snackbar.show();
+                        }
 
                     }
-                })
-                .create();
+                });
+            }
+        });
+
+        return dialog;
     }
 
 
